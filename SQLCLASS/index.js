4f18be0a6a0a75eb989cc713ccce26ee2333ac32
Dileep Yadav <dy695354612@gmail.com>
2026-09-13 19:35:49 +0530

    sql

--- SQLCLASS/index.js
+++ SQLCLASS/index.js
@@ -3,7 +3,10 @@ const mysql = require('mysql2');
 const express = require("express");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
@@ -63,7 +66,35 @@ app.get("/user/:id/edit", (req, res) => {
   try {
     connection.query(q, (err, result) => {
     if(err) throw err;
-    res.render("edit.ejs");
+    let user = (result[0]);
+    res.render("edit.ejs", { user });
+    });
+} catch (err) {
+    console.log(err);
+    res.send("some error in DB");
+  }
+});
+
+// UPDATE (DB) Route
+app.patch("/user/:id", (req, res) => {
+  let { id } = req.params;
+  let {password: formPass, username: newUsername} = req.body;
+  let q = `SELECT * FROM user WHERE id='${id}'`;
+
+   try {
+    connection.query(q, (err, result) => {
+    if(err) throw err;
+    let user = (result[0]);
+    if(formPass != user.password) {
+      res.send("WRONG password");
+    } else {
+      let q2 = `UPDATE user SET username='${newUsername}' WHERE id='${id}'`;
+      connection.query(q2, (err, result) => {
+        if(err) throw err;
+        res.redirect("/user");
+      });
+    }
+    res.send(user);
     });
 } catch (err) {
     console.log(err);
