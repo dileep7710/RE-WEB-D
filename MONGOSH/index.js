6c700a434dfa19263d6a0ddd817e190bf956b178
Dileep Yadav <dy695354612@gmail.com>
2026-09-20 08:25:33 +0530

    mongoDB

--- MONGOSH/index.js
+++ MONGOSH/index.js
@@ -10,6 +10,7 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/test');
 } 
 
+
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
@@ -18,12 +19,32 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-User.insertMany([
-    {name: "Tony", email: "tony@gmail", age: 50},
-    {name: "Peter", email: "peter@gmail", age: 30},
-    {name: "Tony", bruce: "bruce@gmail", age: 47},
 
-]);
+
+User.deleteMany({name: "Eve"}).then((res) => {
+    console.log(res);
+});
+
+// User.updateMany({age: {$gt: 48} }, {age: 55})
+// .then((res) => {
+//     console.log(res);
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
+// User.findOne({ age: { $gt: 47 } }).then((res) => {
+//     console.log(res[0]);
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
+
+// User.insertMany([
+//     {name: "Tony", email: "tony@gmail", age: 50},
+//     {name: "Peter", email: "peter@gmail", age: 30},
+//     {name: "Tony", bruce: "bruce@gmail", age: 47},
+
+// ]);
 
 // const user1 = new User({
 //     name: "Adam",
