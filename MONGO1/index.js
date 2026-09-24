780a2a84a663668b47cfaae25829b0dd57066ac6
Dileep Yadav <dy695354612@gmail.com>
2026-09-24 07:23:29 +0530

    Whatsapp

--- MONGO1/index.js
+++ MONGO1/index.js
@@ -2,17 +2,27 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 
-main().then(() => {console.log("connection successful")})
+main().then(() => {console.log("connection successful");
+
+})
 .catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp'); 
 }
+
+// Index Route
+app.get("/chats", async (req, res) => {
+    let chats = await Chat.find();
+    console.log(chats);
+    res.render("index.ejs", {chats});
+});
 
 app.get("/", (req, res) => {
     res.send("root is working");
