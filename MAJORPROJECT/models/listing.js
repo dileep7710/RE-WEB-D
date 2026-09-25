8463153714e99043fdab57aa4a567611a2d2872e
Dileep Yadav <dy695354612@gmail.com>
2026-09-25 17:28:45 +0530

    majorProject

---
+++ MAJORPROJECT/models/listing.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+
+    description: String,
+    image: {
+        type: String,
+        default:
+            "https://unsplash.com/photos/a-vibrant-sunset-over-a-hazy-landscape-tGp2wneBC8c",
+        set: (v) => 
+            v === "" 
+              ? "https://unsplash.com/photos/a-vibrant-sunset-over-a-hazy-landscape-tGp2wneBC8c"
+              : v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
